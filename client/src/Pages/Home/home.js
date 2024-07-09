6246f1ce6d3fb69524db467b6a2ba9949e8de7ac
Diwas Sapkota <diwassapkota805@gmail.com>
2024-07-09 17:25:23 -0500

    updated landing page

--- client/src/Pages/Home/home.js
+++ client/src/Pages/Home/home.js
@@ -7,19 +7,26 @@ const Home = () => {
 
     const handleGetStarted = () => {
         console.log("Get started button clicked");
-        navigate('/tripInputForm');
+        navigate('/register');
     }
     return (
         <div className="home">
-            <div className="home-left">
-                <h1 className="home-title">TourEZ</h1>
-                <h2 className="home-subtitle">Your Next Journey, Optimized</h2>
-                <p className="home-description">Embark on seamless travel experience with TourEZ, your go-to web application for effortless trip plannign. Craft personalized itineaires, explore exciting destinations, and make every journey memorable. Start your adventure with TourEZ today!</p>
-                <button className="form-button" onClick={handleGetStarted}>Get Started</button>
+            <div className="home-top">
+                <div className="home-left">
+                    <h1 className="home-title">TourEZ</h1>
+                    <h2 className="home-subtitle">Your Next Journey, Optimized</h2>
+                    <p className="home-description">Embark on seamless travel experience with TourEZ, your go-to web application for effortless trip plannign. Craft personalized itineaires, explore exciting destinations, and make every journey memorable. Start your adventure with TourEZ today!</p>
+                    <button className="form-button" onClick={handleGetStarted}>Get Started</button>
+                </div>
+                <div className="home-right">
+                    <div className="hero-image"></div>
+                </div>
             </div>
-            <div className="home-right">
-                <div className="hero-image"></div>
+            <div className="divider"></div>
+            <div className="footer">
+                <p className="footer-text">© 2023 TourEZ. All rights reserved.</p>
             </div>
+
         </div>
     );
 };
